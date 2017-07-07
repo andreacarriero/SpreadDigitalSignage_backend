fa75ef4d07f9aa0f1fb848d56c7870d60bc5c353
Andrea Carriero <andrea@btbh.net>
2017-07-07 16:07:08 +0200

    Log to stdout

--- util/logger.py
+++ util/logger.py
@@ -1,2 +1,8 @@
-import logging
-logging.basicConfig(filename='data/app.log', filemode='a', level=logging.DEBUG)
+import logging, sys
+
+##Log to file
+#logging.basicConfig(filename='data/app.log', filemode='a', level=logging.DEBUG)
+
+#Log to stdout
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
