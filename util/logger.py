d5c77231810b3fbc847e8a4d8884bd61981d8a12
Andrea Carriero <andrea@btbh.net>
2017-09-07 16:36:00 +0200

    deleted logging file handler

--- util/logger.py
+++ util/logger.py
@@ -1,9 +1,8 @@
-import logging, sys, os
+import logging, sys
 
-os.system('touch data/app.log')
-file_handler = logging.FileHandler(filename='data/app.log')
+#file_handler = logging.FileHandler(filename='data/app.log')
 stdout_handler = logging.StreamHandler(sys.stdout)
-handlers = [file_handler, stdout_handler]
+handlers = [stdout_handler]
 
 logging.basicConfig(
     level=logging.DEBUG, 
