6508c958eadb163e2920c9d5ec3a3984b91444cf
Andrea Carriero <andrea@btbh.net>
2017-09-08 00:42:35 +0200

    removed token authentication for docs

--- v1/api.py
+++ v1/api.py
@@ -48,7 +48,6 @@ def token_required(f):
 
 
 @app.route('/docs')
-@token_required
 def render_docs():
     log.info('Rendering api docs')
     return '<!DOCTYPE html><html><head><title>API Docs</title><meta charset="utf-8"/><meta name="viewport" content="width=device-width, initial-scale=1"><style>body {margin: 0; padding: 0;}</style></head><body><redoc spec-url="spec.json"></redoc><script src="https://rebilly.github.io/ReDoc/releases/latest/redoc.min.js"> </script></body></html>'
